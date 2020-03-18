7ac3e9a26e687b51a4a3aac479eea34ceeb628db
Josimar de Cunha Ferreira <61252924+josimarcf-new@users.noreply.github.com>
2020-03-17 21:48:03 -0300

    criando tabelas

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import CharField
 
-# Create your models here.
+class Evento(models.Model):
+    titulo: CharField = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do evento')
+    data_criacao = models.DateTimeField(verbose_name='Data', auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
